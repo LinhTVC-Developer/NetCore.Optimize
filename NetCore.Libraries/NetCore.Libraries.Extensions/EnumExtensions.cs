4014564b935b44722f705bc59d1d5f4e612339f0
linhtvc <linhtvc@vnpay.vn>
2025-01-14 18:19:40 +0700

    add new NetCore.Libraries.Extensions

---
+++ NetCore.Libraries/NetCore.Libraries.Extensions/EnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace NetCore.Libraries.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string EnumDescription(this Enum value)
+        {
+            if(value == null)
+            {
+                return "";
+            }
+            FieldInfo fieldInfo = value?.GetType().GetField(value?.ToString());
+
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return (attributes != null && attributes.Length > 0) ? attributes[0].Description : value.ToString();
+        }
+    }
+}
