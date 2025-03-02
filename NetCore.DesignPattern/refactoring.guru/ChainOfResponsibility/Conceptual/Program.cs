5648c991cb75753b1eaabdcc676e979b343462ca
DESKTOP-QASAIU9\Lenovo <linhtvc.dev@gmail.com>
2025-03-03 06:23:10 +0700

    add refactoring.guu

---
+++ NetCore.DesignPattern/refactoring.guru/ChainOfResponsibility/Conceptual/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace refactoring.guru.ChainOfResponsibility.Conceptual
+{
+    public interface IHandler
+    {
+
+    }
+
+    public abstract class AbstractHandler: IHandler
+    {
+
+    }
+
+    public class MoneyHandler : AbstractHandler
+    {
+
+    }
+
+    public class SquirrelHandle : AbstractHandler
+    {
+
+    }
+
+    public class DogHandler : AbstractHandler
+    {
+
+    }
+
+    public class Client
+    {
+
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
